e47b06856d7dbcf0c68c9a13df7376415753521d
Kaique Prado <kiqprado@outlook.com>
2024-04-15 06:19:11 -0300

    feat: Text Auth

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,29 +1,18 @@
 const { hash, compare } = require('bcryptjs')
-
 const AppError = require('../utils/AppError')
 
 const sqliteConnection = require('../database/sqlite')
+const UserRepository = require('../repositories/UserRepository')
+const UserCreateService = require('../services/UserCreateService')
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
-    const database = await sqliteConnection()
-    const checkUsersExist = await database.get(
-      'SELECT * FROM users WHERE email = (?)',
-      [email]
-    )
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
 
-    if (checkUsersExist) {
-      throw new AppError('Este e-mail já está em uso.')
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await database.run(
-      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-      [name, email, hashedPassword]
-    )
+    userCreateService.execute({ name, email, password })
 
     return response.status(201).json()
   }
